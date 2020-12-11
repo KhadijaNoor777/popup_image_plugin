028e42c10d8527411c1b86b3d92280bd3cf837d5
KhadijaNoor777 <khadijanoor1405@gmail.com>
2020-12-11 20:28:49 +0500

    Updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $("#imgGallery").imagePopup({
     overlayColor : "rgba(0, 0, 0, 0.5)",
     closeButtonImg : {
-        src : "../images/close.png",
+        src : "./images/close.png",
         width : "40px",
         height : "40px"
     },
